87a46cb6eaada3b9a250fa597f316d65889db551
Logdone <Firas.bouchami@gmail.com>
2018-10-05 21:54:03 +0100

    First Commit. Project Upload.

---
+++ FME/src/main/java/com/fme/ftmf/FME/FmeApplication.java
@@ -0,0 +1,12 @@
+package com.fme.ftmf.FME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmeApplication.class, args);
+	}
+}
